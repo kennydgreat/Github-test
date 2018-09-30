4de808f0f88361e6d724b1dbcc0fdfd044c2af69
Kenneth Uyabeme <kuyabeme@gmail.com>
2018-09-29 23:46:41 -0400

    test commit

--- Github test/Controllers/HomeController.cs
+++ Github test/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Github_test.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description sdsfs.";
 
             return View();
         }
